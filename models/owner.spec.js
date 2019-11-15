fcf30b8e6c260367f7de71aac83d48221f82d84d
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-14 18:07:39 -0600

    testing owner models II

--- models/owner.spec.js
+++ models/owner.spec.js
@@ -4,14 +4,11 @@ const bcrypt = require('bcryptjs')
 
 describe('Owner Model', () => {
 
-
-    describe('get()', () =>{
-
         beforeEach(async () => {
             await db('owners').truncate()
         })
 
-        it('inserts new owner Jane', async () => {
+        it('inserts() new owner Jane', async () => {
             const owner = await insert({
                 username: 'Jane', 
                 password: bcrypt.hashSync("12345", 10),
@@ -27,5 +24,28 @@ describe('Owner Model', () => {
             expect(owner.email).toBe('jane.eyre@gmail.com')
             expect(owner.id).toBeDefined();
         })
-    })
+        it('findById(2)', async () => {
+            await insert({
+                username: 'Jane', 
+                password: bcrypt.hashSync("12345", 10),
+                name: 'Jane Eyre',
+                residence: 'Texas',
+                email: 'jane.eyre@gmail.com'
+            })
+
+            await insert({
+                username: 'Lilly01', 
+                password: bcrypt.hashSync("12345", 10),
+                name: 'Lilly Eyre',
+                residence: 'Texas',
+                email: 'lilly.eyre@gmail.com'
+            })
+
+            const response = await findById(2)
+            expect(response.id).toBe(2)
+
+            // const owners = await db('owners')
+
+        })
+
 })
